eb24c23b39ee193a2eaa3e3b3d1380c31a78b10b
FivePixels <o5pxels@gmail.com>
2022-10-05 12:57:19 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use rand::Rng;
+use std::include_bytes;
+
+fn main() {
+    let bytes = include_bytes!("resources/fortunes");
+    let fortunes_as_string = String::from_utf8_lossy(bytes);
+    let fortunes: Vec<&str> = fortunes_as_string.split("\n").collect();
+    let mut rng = rand::thread_rng();
+    let rn = rng.gen_range(0..fortunes.len());
+    println!("{}", &fortunes[rn]);
+}
