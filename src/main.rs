19e25255a94c2386e5746e3a60206628e40de578
FivePixels <o5pxels@gmail.com>
2022-10-05 15:05:35 -0500

    Fix rng range

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ fn main() {
     let fortunes_as_string = String::from_utf8_lossy(bytes);
     let fortunes: Vec<&str> = fortunes_as_string.split("\n").collect();
     let mut rng = rand::thread_rng();
-    let rn = rng.gen_range(0..fortunes.len());
+    let rn = rng.gen_range(0..fortunes.len() - 1);
     println!("{}", &fortunes[rn]);
 }
